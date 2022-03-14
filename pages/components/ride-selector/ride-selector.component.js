65ea34a4b3485c46c488ff610fa1239e3f659893
An Tran <hoangan08ece@gmail.com>
2022-03-14 13:01:24 +0100

    put carList in ride-selector

--- pages/components/ride-selector/ride-selector.component.js
+++ pages/components/ride-selector/ride-selector.component.js
@@ -1,6 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { carList } from "../../data/carList";
 import tw from "tailwind-styled-components";
+
+const carList = [
+  {
+    imgUrl: "https://i.ibb.co/cyvcpfF/uberx.png",
+    service: "UberX",
+    multiplier: 1, // price in $ per minute -> $1*3 = $3 per minute
+  },
+  {
+    imgUrl: "https://i.ibb.co/YDYMKny/uberxl.png",
+    service: "UberXL",
+    multiplier: 1.5,
+  },
+  {
+    imgUrl: "https://i.ibb.co/Xx4G91m/uberblack.png",
+    service: "Black",
+    multiplier: 2,
+  },
+  {
+    imgUrl: "https://i.ibb.co/cyvcpfF/uberx.png",
+    service: "Comfort",
+    multiplier: 1.2, // $1.2*3 = $3.6 per minute
+  },
+  {
+    imgUrl: " https://i.ibb.co/1nStPWT/uberblacksuv.png",
+    service: "Black SUV",
+    multiplier: 2.8,
+  },
+];
 
 const RideSelector = ({ pickupCoordinates, dropoffCoordinates }) => {
   const [rideDuration, setRideDuration] = useState(0); // rideDuration is how long it takes to go from point a to point b
